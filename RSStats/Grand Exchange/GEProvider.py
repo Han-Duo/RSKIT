cde7522f57e3e3970e37ffb2696d77d7a3f8443a
IAmOnYourSide <du.yuehan@gmail.com>
2015-12-12 00:25:15 -0500

    getHistoricalDataUrl

--- RSStats/Grand Exchange/GEProvider.py
+++ RSStats/Grand Exchange/GEProvider.py
@@ -1,1 +1,18 @@
-#GEProvider.py
+# GEProvider.py
+# hansololz HanSeoulOh
+# han-duo
+
+
+C_CURDATASOURCE = """http://services.runescape.com/m=itemdb_rs/api/"""
+C_HISTORICALDATASOURCEBEGIN = """http://runescape.wikia.com/wiki/Module:Exchange/"""
+C_HISTORICALDATASOURCEEND = """/Data"""
+
+
+def getHistoricalDataUrl(itemname):
+	return """{wikibegin}{itemname}{wikiend}""".format(
+		wikibegin = C_HISTORICALDATASOURCEBEGIN,
+		itemname = itemname,
+		wikiend = C_HISTORICALDATASOURCEEND)
+
+
+getHistoricalDataUrl("test")
